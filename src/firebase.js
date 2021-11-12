1d11bce962d5113ebafcf1355ea196845b7ed344
HaoChuanLee <kb274483@gmail.com>
2021-11-12 15:18:18 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyCdhzEQzfQhzyLqXtA7000QZw7zuVLv5FQ",
-  authDomain: "tictactoe-webgame-677ca.firebaseapp.com",
-  databaseURL: "https://tictactoe-webgame-677ca-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tictactoe-webgame-677ca",
-  storageBucket: "tictactoe-webgame-677ca.appspot.com",
-  messagingSenderId: "163943786800",
-  appId: "1:163943786800:web:af8249c681f67c92a880f5",
-  measurementId: "G-F8Z73VQMV3"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "https://tictactoxxxxxxxxxxxxxxxsia-southeast1.firebasedatabase.app",
+  projectId: "txxxxxxxxxxxxx77ca",
+  storageBucket: "tixxxxxxxxxxxxxxxpot.com",
+  messagingSenderId: "xxxx94xxxxxxxxx",
+  appId: "1:1xxxxxxxxxxxxxxxxxxxxxxx880f5",
+  measurementId: "xxx-ZxxxQx3"
 };
 
 firebase.initializeApp(config);
